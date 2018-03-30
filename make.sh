176b56ea1a5cfd4a7e7abee74de6df19fccc53f9
kubotan <admin@gorogoro.space>
2018-03-31 08:51:14 +0900

    Initial Commit

--- make.sh
+++ make.sh
@@ -5,5 +5,5 @@ make USE_LIBV4L2=true clean all
 make install
 cp ./mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
 systemctl daemon-reload
-systemctl enable motioneye
-systemctl start motioneye
+systemctl enable mjpg_streamer
+systemctl start mjpg_streamer
