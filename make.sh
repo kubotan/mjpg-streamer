d1fa388ed1de3915de066d815580c45ea59e50b6
kubotan <admin@gorogoro.space>
2018-03-31 08:47:46 +0900

    Initial Commit

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get install git libjpeg-dev imagemagick cmake checkinstall libv4l-dev
 make USE_LIBV4L2=true clean all
 make install
 cp ./mjpg_streamer.service /etc/systemd/system/mjpg_streamer.service
