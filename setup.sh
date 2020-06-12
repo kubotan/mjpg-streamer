84b6c4bd225f630f89376d2d03568e0200aa2d3f
kubotan <admin@gorogoro.space>
2020-06-12 12:58:52 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ apt-get install git libjpeg-dev imagemagick cmake checkinstall libv4l-dev
 make USE_LIBV4L2=true clean all
 make install
 crontab -l > crontabs.txt
-echo '@reboot /usr/local/bin/mjpg_streamer -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -n -q 100' -o 'output_http.so -p 8080' -o 'output_motion.so --foreground-diff-threshold 35 --background-learning-rate 0.01 --motion-noise-threshold 0.002' >> crontabs.txt
-echo '@reboot /usr/local/bin/mjpg_streamer -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video1 -n -q 100' -o 'output_http.so -p 8081' -o 'output_motion.so --foreground-diff-threshold 35 --background-learning-rate 0.01 --motion-noise-threshold 0.002' >> crontabs.txt
+echo "@reboot /usr/local/bin/mjpg_streamer -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -n -q 100' -o 'output_http.so -p 8080' -o 'output_motion.so --foreground-diff-threshold 35 --background-learning-rate 0.01 --motion-noise-threshold 0.002" >> crontabs.txt
+echo "@reboot /usr/local/bin/mjpg_streamer -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video1 -n -q 100' -o 'output_http.so -p 8081' -o 'output_motion.so --foreground-diff-threshold 35 --background-learning-rate 0.01 --motion-noise-threshold 0.002" >> crontabs.txt
 crontab crontabs.txt
