9b00ad171a5f001256db6c592f338f4c103a3b4c
kubotan <admin@gorogoro.space>
2020-06-12 14:27:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install git libjpeg-dev imagemagick cmake checkinstall libv4l-dev
+apt-get install libjpeg-dev imagemagick cmake checkinstall libv4l-dev
 make USE_LIBV4L2=true clean all
 make install
 crontab -l > crontabs.txt
