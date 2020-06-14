1213fcfbe8b7fa0e592d381c2e20cf3344d1dc7d
kubotan <admin@gorogoro.space>
2020-06-14 22:02:54 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ make install
 crontab -l > crontabs.txt
 echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -n -q 100' -o 'output_http.so -p 8080'" >> crontabs.txt
 #echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video1 -n -q 100' -o 'output_http.so -p 8081'" >> crontabs.txt
+#echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1920x1080 -d /dev/video0 -y -n -q 100' -o 'output_http.so -p 8080'" >> crontabs.txt
 crontab crontabs.txt
 rm -f crontabs.txt
