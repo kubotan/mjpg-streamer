dd9cfb47a78450a8f07b9472df33de5bf76e8ab1
kubotan <admin@gorogoro.space>
2020-06-15 02:29:30 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ apt-get install libjpeg-dev imagemagick cmake checkinstall libv4l-dev
 make USE_LIBV4L2=true clean all
 make install
 crontab -l > crontabs.txt
+#echo "@reboot ifdown wlan0" >> crontabs.txt
 echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video0 -n -q 100' -o 'output_http.so -p 8080'" >> crontabs.txt
 #echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1280x720 -d /dev/video1 -n -q 100' -o 'output_http.so -p 8081'" >> crontabs.txt
 #echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1920x1080 -d /dev/video0 -y -n -q 100' -o 'output_http.so -p 8080'" >> crontabs.txt
