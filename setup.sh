c900f1d37a37e9372269b371a1d2ae108e18a326
kubotan <admin@gorogoro.space>
2020-06-17 04:17:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get install git
+git clone https://github.com/kubotan/mjpg-streamer ./mjpg-streamer
+cd mjpg-streamer
 apt-get install libjpeg-dev imagemagick cmake checkinstall libv4l-dev
 make USE_LIBV4L2=true clean all
 make install
@@ -9,3 +12,4 @@ echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1280x720
 #echo "@reboot /usr/local/bin/mjpg_streamer -b -i 'input_uvc.so -f 30 -r 1920x1080 -d /dev/video0 -y -n -q 100' -o 'output_http.so -p 8080'" >> crontabs.txt
 crontab crontabs.txt
 rm -f crontabs.txt
+reboot
